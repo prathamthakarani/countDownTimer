ed7575daa2ecef1ba1a4b4ae0029c5ce3a9b0a92
prathamthakarani <thakaranipratham@gmail.com>
2022-09-15 00:03:45 -0800

    script.js

--- script.js
+++ script.js
@@ -22,7 +22,16 @@ while(i<allButtons.length)
 }
 function backAt() {
   let minutes = Math.floor(valueOfTime / 60);
-  let currentHours=new Date().getHours();
+  displayTime(minutes);
+}
+
+function inputBackAt(inputMin){
+    let minutes = Math.floor((inputMin/ 60));
+    displayTime(minutes);
+  }
+  
+function displayTime(minutes){
+    let currentHours=new Date().getHours();
   let currentMin= new Date().getMinutes();
   let newMin = currentMin + minutes;
   if (newMin > 59) {
@@ -36,15 +45,7 @@ function backAt() {
 }
 
 let displayTimeLeft = setInterval(function () {
-  let minutes = Math.floor(valueOfTime / 60);
-  let seconds = valueOfTime % 60;
-  if (valueOfTime > 0) {
-    document.getElementById("display__time-left").innerHTML = minutes + ":" + seconds;
-  }
-  else if(valueOfTime == 0){
-    document.getElementById("display__time-left").innerHTML = "00:00"; 
-    alert("Your CountDown Timer is at 00:00");
-  }
+  timeLeft(valueOfTime);
   valueOfTime--;
 }, 1000);
 
@@ -57,32 +58,22 @@ document.getElementById("customMin")
       let inputSec = document.getElementById("customMin").value *60;
       inputBackAt(inputSec);
       setInterval(function () {
-        let minutes = Math.floor((inputSec / 60));
-        let seconds = inputSec % 60;
-        if (inputSec > 0) {
-          document.getElementById("display__time-left").innerHTML =
-            minutes + ":" + seconds;
-        }
-        else if(inputSec == 0){
-          document.getElementById("display__time-left").innerHTML = "00:00";
-          alert("Your CountDown Timer is at 00:00");
-        }
+        timeLeft(inputSec);
         inputSec--;
       }, 1000);
     }
   });
 
-function inputBackAt(inputMin){
-  let minutes = Math.floor((inputMin/ 60));
-  let currentHours=new Date().getHours();
-  let currentMin= new Date().getMinutes();
-  let newMin = currentMin + minutes;
-  if (newMin> 59) {
-    let remainingMinutes = newMin - 60;
- document.getElementById('display__end-time').innerHTML= `Be Back At ${currentHours+1} : ${remainingMinutes}`;
+  function timeLeft(sec){
+    let minutes = Math.floor(sec / 60);
+    let seconds = sec % 60;
+    if (sec > 0) {
+      document.getElementById("display__time-left").innerHTML = minutes + ":" + seconds;
+    }
+    else if(sec == 0){
+      document.getElementById("display__time-left").innerHTML = "00:00"; 
+      alert("Your CountDown Timer is at 00:00");
+    }
+    sec--;
   }
-  else{
-    let remainingMin = currentMin + minutes;
-    document.getElementById('display__end-time').innerHTML= `Be Back At ${currentHours} : ${remainingMin}`;
-  }
-}
+
